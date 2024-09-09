8e1fe046a3a767237d4c1f3aa2c7be93989ae739
chowan kumar <chowanmalhani5@gmail.com>
2024-09-10 04:08:06 +0500

    complete the soeciality page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
+import Speaciality from '../components/Speaciality'
 
 const Home = () => {
   return (
     <div>
     
       <Header/>
+      <Speaciality/>
     </div>
   )
 }
