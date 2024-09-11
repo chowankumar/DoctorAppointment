53077c0f706608763fff544f33c91483ecdfc4c3
chowan kumar <chowanmalhani5@gmail.com>
2024-09-11 11:03:07 +0500

    comlete the banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Speaciality from '../components/Speaciality'
 import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Header/>
       <Speaciality/>
       <TopDoctors/>
+      <Banner/>
     </div>
   )
 }
