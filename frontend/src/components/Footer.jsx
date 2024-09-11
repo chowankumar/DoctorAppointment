fcb4e777aa2aa235c377784fc68bb4cffaa5598e
chowan kumar <chowanmalhani5@gmail.com>
2024-09-11 11:21:14 +0500

    complete the footer componenet and aslo home page

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+            <div>
+                <img  className='mb-5 w-40' src={assets.logo} alt="" />
+                <p className='flex flex-col gap-2 text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa ducimus ullam alias molestias quas, commodi doloribus placeat hic quod quia!</p>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>Company</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
+                    <li>+92 3499435773</li>
+                    <li>chowanmalhani5@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2024@ Chowan.com. All Right Reserved</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
