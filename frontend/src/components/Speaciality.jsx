6c85ecbba4c4cdc8f2290bc7d958929dad7d092c
chowan kumar <chowanmalhani5@gmail.com>
2024-09-11 13:49:42 +0500

    complete the doctor page

--- frontend/src/components/Speaciality.jsx
+++ frontend/src/components/Speaciality.jsx
@@ -13,7 +13,7 @@ const Speaciality = () => {
                 {
                     specialityData.map((item, index) =>
                     (
-                        <Link onClick={()=> scrollTo(0,0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctor/${item.speciality}`}>
+                        <Link onClick={()=> scrollTo(0,0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${item.speciality}`}>
                             <img className='w-16 sm:w-24 mb-2' src={item.image} alt="" />
                             <p>{item.speciality}</p>
 
