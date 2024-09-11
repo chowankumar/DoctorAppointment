d3c785b13a92d09edf366572084d65b360e2926a
chowan kumar <chowanmalhani5@gmail.com>
2024-09-11 17:51:09 +0500

    complete doctor and appointment page

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,21 +1,31 @@
-import React, { useContext } from 'react'
- 
-import { useNavigate } from 'react-router-dom'
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { doctors } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
-const TopDoctors = () => {
-    const navigate = useNavigate();
-    const{doctors} = useContext(AppContext)
-    return (
-        <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
+const RelatedDoctors = ({speciality,docId}) => {
+    const {doctos} = useContext(AppContext);
+    const [relDoc,SetRelDocs]= useState([]);
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(doctors.length>0 && speciality){
+            const doctorData = doctors.filter((doc)=> doc.speciality === speciality && doc._id !== docId)
+            SetRelDocs(doctorData)
+        }
+
+    },[doctors,speciality,docId])
+  return (
+    <div>
+         <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
             <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
             <p className='sm:w-1/3 text-center text-sm'>simply browse through our extensive list of trusted doctor</p>
             <div className='w-full grid 
             grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
                 {
-                    doctors.slice(0, 10).map((item, index) => (
+                    relDoc.slice(0,5).map((item, index) => (
 
-                        <div key={index}  onClick={()=> {navigate(`/appointment/${item._id}`);scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
+                        <div key={index} onClick={()=> {navigate(`/appointment/${item._id}`);scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
                             <img
                                 className='bg-blue-50'
                                 src={item.image} alt="" />
@@ -32,7 +42,9 @@ const TopDoctors = () => {
             </div>
             <button onClick={()=> navigate('/doctors')} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
         </div>
-    )
+          
+    </div>
+  )
 }
 
-export default TopDoctors
+export default RelatedDoctors
