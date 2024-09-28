89e505560efd861ea12962cf8ea946f5d7d99138
chowan kumar <chowanmalhani5@gmail.com>
2024-09-28 09:50:12 -0700

    create function which fetch the all doctors

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,14 +1,32 @@
 import { createContext, useState } from "react";
-
+import {toast} from 'react-toastify'
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
     const [aToken,setAtoken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
+    const[doctors,setDoctors] = useState([])
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+    const getAllDoctors = async()=>{
+        try {
+            const {data} = await axios.post(backendUrl + '/api/admin/all-doctors',{},{headers:{aToken}})
+
+            if(data.success){
+                setDoctors(data.doctors)
+            }else{
+                toast.error(error.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+            
+        }
+
+    }
 
     const value = {
         aToken,setAtoken,
-        backendUrl,
+        backendUrl,getAllDoctors,
+        doctors
 
     }
 
