d6144ce2451db68d354913535056134a9a4d86d0
chowan kumar <chowanmalhani5@gmail.com>
2024-09-28 04:34:52 -0700

    set the routes of the pages in admin section

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react'
 import Login from './pages/Login'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
-
+import Sidebar from './components/Sidebar';
+import {Routes,Route } from 'react-router-dom';
+import Dashboard from './pages/Admin/AddDoctor'
+import  AllApointmetns from './pages/Admin/AllApointments'
+import AddDoctor from './pages/Admin/AddDoctor'
+import DoctorList from './pages/Admin/DoctorsList'
 
 const App = () => {
   const {aToken} = useContext(AdminContext)
@@ -13,6 +18,17 @@ const App = () => {
     <div className='bg-[#F8F9FD]'>
       <ToastContainer />
       <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar/>
+        <Routes>
+          <Route path='/' element={<></>}/>
+          <Route path='/admin-dashboard' element={<Dashboard />}/>
+          <Route path='/all-appointments' element={<AllApointmetns/>}/>
+          <Route path='/add-doctor' element={<AddDoctor/>}/>
+          <Route path='/doctor-list' element={<DoctorList/>}/>
+
+        </Routes>
+      </div>
 
     </div>
   ) : (
