8ff981b72a9f44325bc5aa62f95845ab2d5602a7
chowan kumar <chowanmalhani5@gmail.com>
2024-09-28 03:20:16 -0700

    create the navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
 
     <div className='bg-[#F8F9FD]'>
       <ToastContainer />
+      <Navbar/>
 
     </div>
   ) : (
