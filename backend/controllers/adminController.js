52c2a0b53d707502c0e0104e0e1bc73bb3a2c0e3
chowan kumar <chowanmalhani5@gmail.com>
2024-09-28 09:43:50 -0700

    create conroller to fetch th all doctors

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -78,4 +78,17 @@ const loginAdmin = async(req,res)=>{
 
 }
 
-export  {addDoctor,loginAdmin}
+
+//api to get the all doctors/////
+
+const allDoctors = async(req,res)=>{
+    try {
+        const doctors = await doctorModel.find({}).select('-password')
+        res.json({success:true,doctors})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+        
+    }
+}
+export  {addDoctor,loginAdmin,allDoctors}
