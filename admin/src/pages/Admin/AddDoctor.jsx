08d48ad700095235f38ab3154fa94bda1f98fbb3
chowan kumar <chowanmalhani5@gmail.com>
2024-10-01 14:15:23 -0700

    update the profile function done

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -41,9 +41,7 @@ const AddDoctor = () => {
       formData.append('fees',fees)
       formData.append('address',JSON.stringify({line1:address1,line2:address2}))
 
-      formData.forEach((value,key)=>{
-        console.log(`${key} :: ${value}`)
-      })
+    
     
       const {data} = await axios.post(backendUrl + '/api/admin/add-doctor',formData,{headers:{aToken}})
 
