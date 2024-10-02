ab14a185b5e267ed85b52c6e22d6abce4b01243f
chowan kumar <chowanmalhani5@gmail.com>
2024-10-02 01:53:18 -0700

    complete the appointment section complete where use select the time slot and book appointment slot

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -59,11 +59,27 @@ const Appointment = () => {
       while (currentDate < endTime) {
         let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
-        // Add the slot to the array
+
+
+        let day = currentDate.getDate();
+        let month = currentDate.getMonth()+1;
+        let year = currentDate.getFullYear()
+
+        const slotDate = day + "_" + month + "_" + year
+        const slotTime = formattedTime;
+
+        const isSlotAvailable = docInfo.slots_booked[slotDate]&& docInfo.slots_booked[slotDate].includes(slotTime) ?false : true;
+
+        if(isSlotAvailable){
+           // Add the slot to the array
         timeSlots.push({
           datetime: new Date(currentDate),
           time: formattedTime
         });
+        }
+
+
+       
 
         // Increment the time by 30 minutes
         currentDate.setMinutes(currentDate.getMinutes() + 30);
@@ -88,14 +104,13 @@ const Appointment = () => {
       let year = date.getFullYear();
 
       const slotDate = day + "_" + month + '_' + year;
-      console.log(slotDate)
         
       const {data} = await axios.post(backendUrl + '/api/user/book-appointment',{docId,slotDate,slotTime},{headers:{token}})
-                
+                console.log(data)
       if(data.success){
         toast.success(data.message);
         getDoctorsData();
-        navigate('/my-appointment')
+        navigate('/my-appointments')
       }else{
          toast.error(data.message)
          console.log(data.message)
