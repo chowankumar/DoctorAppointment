962756194c16184c78bb569cbd0b772e947815ad
chowan kumar <chowanmalhani5@gmail.com>
2024-10-09 11:34:28 -0700

    create api to fetch all the appointments

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt"
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from './../models/doctorModel.js'
 import jwt from "jsonwebtoken"
+import appointmentModel from "../models/appointmentModel.js";
 
 const addDoctor = async (req,res)=>{
     try {
@@ -92,4 +93,19 @@ const allDoctors = async(req,res)=>{
         
     }
 }
-export  {addDoctor,loginAdmin,allDoctors}
+
+//api to get all the appointments
+const appointmentAdmin = async (req,res)=>{
+    try {
+        const appointments = await appointmentModel.find({});
+        res.json({success:true,appointments})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+        
+        
+    }
+}
+
+ 
+export  {addDoctor,loginAdmin,allDoctors,appointmentAdmin}
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {addDoctor,loginAdmin,allDoctors} from './../controllers/adminController.js'
+import {addDoctor,loginAdmin,allDoctors,appointmentAdmin} from './../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import authAdmin  from "../middlewares/authAdmin.js"
 import { changeAvailablity } from "../controllers/doctorController.js"
@@ -10,5 +10,7 @@ adminRouter.post('/add-doctor',authAdmin, upload.single('image'),addDoctor);
 adminRouter.post('/login-admin',loginAdmin);
 adminRouter.post('/all-doctors',authAdmin,allDoctors);
 adminRouter.post('/change-availability',authAdmin,changeAvailablity)
+adminRouter.get('/appointments',authAdmin,appointmentAdmin)
+
 
 export default adminRouter
